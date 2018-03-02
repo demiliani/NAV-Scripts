fc8f36e48b6fa0f859e8553831a09dd4fb798863
demiliani <33155438+demiliani@users.noreply.github.com>
2018-03-02 09:29:49 +0100

    Added Powershell script for running C#

---
+++ NAVJobsWithDLL.ps1
@@ -0,0 +1,38 @@
+﻿$Assem = (
+    "EID.NavTools, Version=2.0.0.0, Culture=neutral, PublicKeyToken=73e9bde131e8729c"
+    ) 
+
+$code = @" 
+using System;
+
+
+namespace EID 
+{    
+    public class MyNAVSuperClass    
+    {        
+        public static void MyStaticMethod()            
+        {            
+            Console.WriteLine("Working with NAV");        
+        }
+        
+        public void MyMethod()        
+        {            
+            Console.WriteLine("Working with NAV 2");        
+        }    
+    } 
+} 
+"@
+
+Add-Type -ReferencedAssemblies $Assem -TypeDefinition $Source -Language CSharp 
+
+
+# Check the type has not been previously added within the session, otherwise an exception is raised 
+if (-not ([System.Management.Automation.PSTypeName]'EID.MyNAVSuperClass').Type) 
+{    
+    Add-Type -TypeDefinition $code -Language CSharp; 
+}
+
+[EID.MyNAVSuperClass]::MyStaticMethod();
+
+$instance = New-Object EID.MyNAVSuperClass; 
+$instance.MyMethod(); 
